1cb79f1d1c977f52f57c406329161877a68eeff5
Renan Fernandes <renzinestuods@gmail.com>
2022-09-20 15:04:44 -0300

    Configura o Provider

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/index.css';
+import Provider from './context/Provider';
 
 ReactDOM.render(
-  <App />,
+  <Provider>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
