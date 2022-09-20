a733d32d889768a4e614e0ced4609e15924c40a3
Renan Fernandes <renzinestuods@gmail.com>
2022-09-20 18:50:53 -0300

    REQUISITO-2: Cria um filtro de texto para a tabela

--- src/componentes/Filter.jsx
+++ src/componentes/Filter.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import tableContext from '../context/tableContext';
 
 export default function Filter() {
+  const { setFilterByName, filterByName: { name } } = useContext(tableContext);
+
+  const onInputChange = ({ target: { value } }) => {
+    setFilterByName({ name: value });
+  };
+
   return (
-    <div>Filter</div>
+    <div>
+      <label htmlFor="filter-name">
+        <input
+          data-testid="name-filter"
+          type="text"
+          id="filter-name"
+          name="name"
+          value={ name }
+          onInput={ onInputChange }
+        />
+      </label>
+    </div>
   );
 }
