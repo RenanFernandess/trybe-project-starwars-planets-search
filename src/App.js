04b6c541a1e732f13ad79195d831316b5d488a8f
Renan Fernandes <renzinestuods@gmail.com>
2022-09-20 15:55:56 -0300

    Rendereza o componente Main. que agora é o responsavel por fazer a requisição para a API

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
-import React, { useEffect, useContext } from 'react';
-import tableContext from './context/tableContext';
-import Table from './componentes/Table';
-import Filter from './componentes/Filter';
+import React from 'react';
 import Provider from './context/Provider';
-
-const URL = 'https://swapi.dev/api/planets';
+import Main from './componentes/Main';
 
 function App() {
-  const { setPlanets } = useContext(tableContext);
-
-  useEffect(() => {
-    fetch(URL)
-      .then((response) => response.json())
-      .then(({ results }) => {
-        const planets = results.map((planet) => {
-          delete planet.residents;
-          return planet;
-        });
-        setPlanets(planets);
-      });
-  }, [setPlanets]);
-
   return (
     <Provider>
-      <main>
-        <Filter />
-        <Table />
-      </main>
+      <Main />
     </Provider>
   );
 }
