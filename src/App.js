741e43094e37f05629adab911bdf024a867af8d0
Renan Fernandes <renzinestuods@gmail.com>
2022-09-20 15:18:37 -0300

    Faz a requisição para a API do star wars

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import tableContext from './context/tableContext';
+import Table from './componentes/Table';
+import Filter from './componentes/Filter';
+
+const URL = 'https://swapi.dev/api/planets';
 
 function App() {
+  const { setPlanets } = useContext(tableContext);
+
+  useEffect(() => {
+    fetch(URL)
+      .then((response) => response.json())
+      .then(({ results }) => setPlanets(results));
+  }, [setPlanets]);
+
   return (
-    <span>Hello, App!</span>
+    <main>
+      <Filter />
+      <Table />
+    </main>
   );
 }
 
