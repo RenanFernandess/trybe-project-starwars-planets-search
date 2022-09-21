fc874d16557aa20b145629e2f60455fb8e28b3f5
Renan Fernandes <renzinestuods@gmail.com>
2022-09-21 16:26:28 -0300

    Realiza a logica de remover o filtro

--- src/componentes/FilterCard.jsx
+++ src/componentes/FilterCard.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import propTypes from 'prop-types';
+import tableContext from '../context/tableContext';
 
 export default function FilterCard({ filter: { column, comparison, value } }) {
+  const { setFilterByNumericValues } = useContext(tableContext);
+
+  const removeFilter = () => {
+    setFilterByNumericValues((preveiusFiltersNumerics) => (
+      preveiusFiltersNumerics.filter(({ column: col }) => col !== column)
+    ));
+  };
+
   return (
     <div data-testid="filter">
       <p>{`${column} ${comparison} ${value}`}</p>
       <button
         type="button"
+        onClick={ removeFilter }
       >
         X
       </button>
