1236ee6bb2888d70f495491ad9ce08a47bd065f2
Renan Fernandes <renzinestuods@gmail.com>
2022-09-22 11:13:56 -0300

    Remove o stado de ordem dos planetas

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -6,19 +6,13 @@ export default function Provider({ children }) {
   const [planets, setPlanets] = useState([]);
   const [filterByName, setFilterByName] = useState({ name: '' });
   const [filterByNumericValues, setFilterByNumericValues] = useState([]);
-  const [order, setOrder] = useState({
-    column: 'population',
-    sort: 'ASC',
-  });
 
   const contextType = {
     filterByName,
     filterByNumericValues,
-    order,
     planets,
     setFilterByName,
     setFilterByNumericValues,
-    setOrder,
     setPlanets,
   };
 
