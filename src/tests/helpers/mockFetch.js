618febc317c8167cd7b05f04b01382a5d5eb4535
Renan Fernandes <renzinestuods@gmail.com>
2022-09-22 14:29:48 -0300

    Corrige um erro de implementação do retorno da API

--- src/tests/helpers/mockFetch.js
+++ src/tests/helpers/mockFetch.js
@@ -1,5 +1,5 @@
 import mockData from '../data/mockData';
 
 export default function mockFetch() {
-  return Promise.resolve({ json: () => Promise.resolve(mockData) });
+  return Promise.resolve({ json: () => Promise.resolve({ results: mockData }) });
 };
