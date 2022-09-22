1bbff7c09110c879d1465ea132d3461d86d3e4a8
Renan Fernandes <renzinestuods@gmail.com>
2022-09-22 15:01:02 -0300

    Verifica se possui um form de filtragem Numerica

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -13,11 +13,19 @@ describe('Testa se é possui campos de filtragem e uma tabela', () => {
     global.fetch.mockClear();
   });
 
-  it('Verifica se possui um input de texto para filtrar por nome', async () => {
-    expect(await screen.findByTestId('name-filter')).toBeInTheDocument();
+  it('Verifica se possui um input de texto para filtrar por nome', () => {
+    expect(screen.getByTestId('name-filter')).toBeInTheDocument();
   })
 
   it('Verifica se possui um form de filtragem Numerica', () => {
+    const inputColumn = screen.getByTestId('column-filter');
+    const inputComparison = screen.getByTestId('comparison-filter');
 
+    expect(screen.getByTestId('value-filter')).toBeInTheDocument();
+    expect(inputColumn).toBeInTheDocument();
+    expect(inputColumn.options).toHaveLength(5);
+    expect(inputComparison).toBeInTheDocument();
+    expect(inputComparison.options).toHaveLength(3);
+    expect(screen.getByRole('button', { name: /Filtrar/i })).toBeInTheDocument();
   });
 });
