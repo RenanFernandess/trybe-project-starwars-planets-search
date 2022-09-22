f9ad35043da7de2e78c367cdcc612e44fea249ff
Renan Fernandes <renzinestuods@gmail.com>
2022-09-22 20:41:09 -0300

    Verifica se a filtragem de "menor que" funciona corretamente

--- src/tests/Filter.test.js
+++ src/tests/Filter.test.js
@@ -30,13 +30,26 @@ describe('Testa se é possivel filtrar os planetas', () => {
     await waitFor(() => expect(screen.getByText(/^Naboo$/i, { selector: 'td' })).toBeInTheDocument());
 
     const naboo = screen.getByRole('cell', { name: /^Naboo$/i });
-    // const inputComparison = screen.getByTestId('comparison-filter');
 
     userEvent.selectOptions(screen.getByTestId('column-filter'), 'rotation_period');
     userEvent.type(screen.getByTestId('value-filter'), '26');
     userEvent.click(screen.getByRole('button', { name: /Filtrar/i }));
 
     expect(screen.getByRole('cell', { name: /^Kamino$/i })).toBeInTheDocument();
+    expect(naboo).not.toBeInTheDocument();
+  });
+
+  it('Verifica se a filtragem de "menor que" funciona corretamente', async () => {
+    await waitFor(() => expect(screen.getByText(/^Naboo$/i, { selector: 'td' })).toBeInTheDocument());
+
+    const naboo = screen.getByRole('cell', { name: /^Naboo$/i });
+
+    userEvent.selectOptions(screen.getByTestId('column-filter'), 'orbital_period');
+    userEvent.selectOptions(screen.getByTestId('comparison-filter'), 'menor que');
+    userEvent.type(screen.getByTestId('value-filter'), '312');
+    userEvent.click(screen.getByRole('button', { name: /Filtrar/i }));
+
+    expect(screen.getByRole('cell', { name: /^Tatooine$/i })).toBeInTheDocument();
     expect(naboo).not.toBeInTheDocument();
   });
 }); 
