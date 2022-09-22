50001ecc6b8a19ef500556e381c8a7129e8d99a8
Renan Fernandes <renzinestuods@gmail.com>
2022-09-22 16:16:00 -0300

    Verifica se os planetas são renderizados na tabela

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -39,10 +39,6 @@ describe('Testa se é possui campos de filtragem e uma tabela', () => {
     expect(screen.getByRole('button', { name: /ordenar/i })).toBeInTheDocument();
   });
 
-  it('Verifica se possui uma tabela', () => {
-
-  });
-
   it('Verifica se a tabela possuir um cabeçalho', () => {
     expect(screen.getByText(/^Name$/i, { selector: 'th' })).toBeInTheDocument();
     expect(screen.getByText(/^Rotation Period$/i, { selector: 'th' })).toBeInTheDocument();
@@ -57,5 +53,18 @@ describe('Testa se é possui campos de filtragem e uma tabela', () => {
     expect(screen.getByText(/^Created$/i, { selector: 'th' })).toBeInTheDocument();
     expect(screen.getByText(/^Edited$/i, { selector: 'th' })).toBeInTheDocument();
     expect(screen.getByText(/^URL$/i, { selector: 'th' })).toBeInTheDocument();
+  });
+
+  it('Verifica se os planetas são renderizados na tabela', async () => {
+    expect(await screen.findByText(/^Tatooine$/i, { selector: 'td' })).toBeInTheDocument();
+    expect(await screen.findByText(/^Alderaan$/i, { selector: 'td' })).toBeInTheDocument();
+    expect(await screen.findByText(/^Yavin IV$/i, { selector: 'td' })).toBeInTheDocument();
+    expect(await screen.findByText(/^Hoth$/i, { selector: 'td' })).toBeInTheDocument();
+    expect(await screen.findByText(/^Dagobah$/i, { selector: 'td' })).toBeInTheDocument();
+    expect(await screen.findByText(/^Bespin$/i, { selector: 'td' })).toBeInTheDocument();
+    expect(await screen.findByText(/^Endor$/i, { selector: 'td' })).toBeInTheDocument();
+    expect(await screen.findByText(/^Naboo$/i, { selector: 'td' })).toBeInTheDocument();
+    expect(await screen.findByText(/^Coruscant$/i, { selector: 'td' })).toBeInTheDocument();
+    expect(await screen.findByText(/^Kamino$/i, { selector: 'td' })).toBeInTheDocument();
   });
 });
