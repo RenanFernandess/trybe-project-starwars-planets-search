bc96050a3c2f4452ef9a421cec76c1706550e152
Renan Fernandes <renzinestuods@gmail.com>
2023-01-17 11:19:04 -0300

    :white_check_mark: test: Verifica se planetas com informações desconhecidas ficam no final da lista ao ordernar em orden crescente

--- src/tests/sort.test.js
+++ src/tests/sort.test.js
@@ -26,24 +26,47 @@ describe('Test se é possivel ordernar os planetas', () => {
     userEvent.click(ascendenteRadio);
     userEvent.click(sortButton);
 
-    const [firstPlanet] = screen.getAllByTestId('planet-name');
+    const planets = screen.getAllByTestId('planet-name');
+    const firstPlanet = planets[0]
+    const lastPlanet = planets[planets.length - 1];
 
     expect(firstPlanet).toHaveTextContent(/bespin/i);
+    expect(lastPlanet).toHaveTextContent(/kamino/i);
   });
 
   it('Verifica se é possivel ordernar em orden decrescente.', async () => {
     await waitFor(() => expect(screen.getByRole('cell', { name: /tatooine/i })).toBeInTheDocument());
 
     const selectInput = screen.getAllByRole('combobox', { name: /coluna/i });
-    const ascendenteRadio = screen.getByRole('radio', { name: /descendente/i });
+    const descendenteRadio = screen.getByRole('radio', { name: /descendente/i });
     const sortButton = screen.getByRole('button', { name: /ordenar/i });
 
     userEvent.selectOptions(selectInput[1], 'rotation_period');
+    userEvent.click(descendenteRadio);
+    userEvent.click(sortButton);
+
+    const planets = screen.getAllByTestId('planet-name');
+    const firstPlanet = planets[0]
+    const lastPlanet = planets[planets.length - 1];
+
+    expect(firstPlanet).toHaveTextContent(/kamino/i);
+    expect(lastPlanet).toHaveTextContent(/bespin/i);
+  });
+
+  it('Verifica se planetas com informações desconhecidas ficam no final da lista ao ordernar em orden crescente.', async () => {
+    await waitFor(() => expect(screen.getByRole('cell', { name: /tatooine/i })).toBeInTheDocument());
+
+    const ascendenteRadio = screen.getByRole('radio', { name: /ascendente/i });
+    const sortButton = screen.getByRole('button', { name: /ordenar/i });
+
     userEvent.click(ascendenteRadio);
     userEvent.click(sortButton);
 
-    const [firstPlanet] = screen.getAllByTestId('planet-name');
+    const planets = screen.getAllByTestId('planet-name');
+    const firstPlanet = planets[0]
+    const lastPlanet = planets[planets.length - 1];
 
-    expect(firstPlanet).toHaveTextContent(/kamino/i);
+    expect(firstPlanet).toHaveTextContent(/yavin iv/i);
+    expect(lastPlanet).toHaveTextContent(/hoth/i);
   });
 });
